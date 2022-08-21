e50382d3bd8b0c41035d45f899177ff33449a17a
weesBees <88573539+weesBees@users.noreply.github.com>
2022-08-22 00:33:48 +0530

    Add files via upload

--- MovingServoForSolar.ino/MovingServoForSolar.ino.ino
+++ MovingServoForSolar.ino/MovingServoForSolar.ino.ino
@@ -1,25 +1,29 @@
-int potentiometerPIN = 2;   
-#include <Servo.h>   
-   // It selects the LED pin  
-int value = 0;     
-  // value initialized to store the coming value from the sensor  
+#include <Servo.h>     
+int roof = 0;  
+int rear=0;    
 int angle1=0;
 int angle2=0;
 Servo servo1;
 Servo servo2;
 void setup()   
 {  
-  // The LED pin is declared as the output pin 
   servo1.attach(8);
   servo2.attach(9); 
   Serial.begin(9600);
 }  
 void loop()   
 {  
-  value = analogRead(potentiometerPIN);    // It reads the value from the sensor  
-  angle1=map(value,0,1023,0,100);
-  angle2=map(value,0,1023,100,0);
-  // Serial.println(angle*-1\);
-  servo1.write(angle1);
-  servo2.write(angle2);
+  roof = analogRead(A0);    
+  rear= analogRead(A1);
+  Serial.print(roof)
+  Serial.print("  ")
+  Serial.println(rear)
+  if(roof>rear){
+  servo1.write(90);
+  servo2.write(0);
+  }
+ else{
+   servo1.write(0);
+   servo2.write(90);
+ }
 }  
